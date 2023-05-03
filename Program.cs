98f4c756655638792a61b1888c28d7139a0ed123
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-03 18:42:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double P1, P2;
+
+            Console.Write("Digite a 1° nota: ");
+            P1 = double.Parse(Console.ReadLine());
+
+            P2 = ((5 * 3) - P1) / 2;
+
+            Console.WriteLine("O aluno tem que tirar {0} na 2° prova para passar", P2);
+        }
+    }
+}
